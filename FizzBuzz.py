3234e34f35a32aadfc2f131defdfbf3f6150b5bb
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-05-14 23:04:05 +0530

    Fizz Buzz

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+
+n = 15
+def fizzBuzz(n):
+    li= []
+    for i  in range ( 1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            li.append("FizzBuzz")
+        elif (i % 3 == 0):
+            li.append("Fizz")
+        elif (i % 5 == 0):
+            li.append("Buzz")
+        else:
+            li.append(str(i))
+    return li
+
+print(fizzBuzz(3))
